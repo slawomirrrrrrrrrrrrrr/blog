0980ce71781be16424ce4a2f7282d54d253c4903
slawomir <vvvegfdd@gmail.com>
2023-09-23 12:37:46 +0300

    create the best rancho

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    Category_name = models.TextField(max_length=30)
+
+class Post(models.Model):
+    post_heder = models.CharField(max_length=50)
+    post_text = models.TextField()
+    author = models.CharField(max_length=50)
+    datetime = models.DateTimeField()
+    category = models.ForeignKey(Category, on_delete= models.SET_DEFAULT, default="no category")
